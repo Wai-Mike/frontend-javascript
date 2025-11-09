a7165b423de3909dd3b92a4ae9253dfaeb488cfc
Wai Mike <waimichael35@gmail.com>
2025-11-09 22:26:37 +0200

    Alias Subjects classes for task 4 checks

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,11 +4,16 @@
 /// <reference path="./subjects/React.ts" />
 /// <reference path="./subjects/Java.ts" />
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+const Cpp = Subjects.Cpp;
+const Java = Subjects.Java;
+const React = Subjects.React;
+type Teacher = Subjects.Teacher;
 
-export const cTeacher: Subjects.Teacher = {
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+export const cTeacher: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
   experienceTeachingC: 10,
